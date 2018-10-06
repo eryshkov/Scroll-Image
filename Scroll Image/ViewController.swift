8765e9ed38cbc1567d357ac611b506fbfa9dc330
eryshkov <wendor265>
2018-10-06 21:56:08 +0300

    Initial Commit

---
+++ Scroll Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scroll Image
+//
+//  Created by Evgeniy Ryshkov on 06/10/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
